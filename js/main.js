fec8f54e9f94f7f79c6c13ae9a2f342f55957c32
Sneyder Martínez Caicedo <sneider.caicedo@gmail.com>
2022-12-14 23:05:39 -0500

    Mis habilidades

--- js/main.js
+++ js/main.js
@@ -30,3 +30,24 @@ navLink.forEach(
     n => n.addEventListener('click', linkAction)
 );
 //======================================================
+
+const [skillsContent, skillsHeader] = [
+    document.getElementsByClassName('skills__content'),
+    document.querySelectorAll('.skills__header')
+];
+
+function toggleSkills() {
+    let itemClass = this.parentNode.className;
+
+    for(let i = 0; i < skillsContent.length; i++) {
+        skillsContent[i].className = 'skills__content skills__close';
+    }
+
+    if(itemClass === 'skills__content skills__close') {
+        this.parentNode.className = 'skills__content skills__open';
+    }
+}
+
+skillsHeader.forEach((element) => {
+    element.addEventListener('click', toggleSkills);
+});
