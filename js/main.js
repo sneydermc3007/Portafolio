26d99faa43fed62b2875e3ab17fdd9b01b360a1b
Sneyder Martínez Caicedo <sneider.caicedo@gmail.com>
2023-01-28 19:37:55 -0500

    Servicios

--- js/main.js
+++ js/main.js
@@ -74,3 +74,28 @@ tabs.forEach(tab => {
         tab.classList.add('qualification__active');
     });
 });
+
+//=====================================================================
+const [modalViews, modalBtns, modalCloses] = [
+    document.querySelectorAll('.services__modal'),
+    document.querySelectorAll('.services__button'),
+    document.querySelectorAll('.services__modal-close')
+];
+
+let modal = function(modalClick) {
+    modalViews[modalClick].classList.add('active-modal');
+}
+
+modalBtns.forEach((modalBtn, i) => {
+    modalBtn.addEventListener('click', () => {
+        modal(i);
+    });
+});
+
+modalCloses.forEach((modalClose) => {
+    modalClose.addEventListener('click', () => {
+        modalViews.forEach((modalView) => {
+            modalView.classList.remove('active-modal');
+        });
+    });
+});
