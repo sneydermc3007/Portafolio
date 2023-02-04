67b74d999e333885cfaea46d484d00a38db46da0
Sneyder Martínez Caicedo <sneider.caicedo@gmail.com>
2023-02-03 23:41:59 -0500

    Problemas al agregar mis proyectos(Swipper)

--- js/main.js
+++ js/main.js
@@ -99,3 +99,17 @@ modalCloses.forEach((modalClose) => {
         });
     });
 });
+
+//===============================================
+// let swiper = new Swiper(".portfolio__container", {
+//     cssMode: true,
+//     loop: true,
+//     navigation: {
+//       nextEl: ".swiper-button-next",
+//       prevEl: ".swiper-button-prev",
+//     },
+//     pagination: {
+//       el: ".swiper-pagination",
+//         clickable: true,
+//     }
+// });
