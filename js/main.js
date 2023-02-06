ba1a803284bf5ec987eab76924505a9d35da4ec3
Sneyder Martínez Caicedo <sneider.caicedo@gmail.com>
2023-02-06 00:05:34 -0500

    Fix: Se arreglo el problema de los dobles swipper horizontales
    Problem: Falta arreglar el tamaño de las imagenes y el posicionamiento de pagination.

--- js/main.js
+++ js/main.js
@@ -101,15 +101,16 @@ modalCloses.forEach((modalClose) => {
 });
 
 //===============================================
-// let swiper = new Swiper(".portfolio__container", {
-//     cssMode: true,
-//     loop: true,
-//     navigation: {
-//       nextEl: ".swiper-button-next",
-//       prevEl: ".swiper-button-prev",
-//     },
-//     pagination: {
-//       el: ".swiper-pagination",
-//         clickable: true,
-//     }
-// });
+let swiper = new Swiper('.portfolio__container', {
+    cssMode: true,
+    loop: true,
+
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+});
