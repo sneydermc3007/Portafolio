8f712c09928c770899d34228cde633fe6436334f
Sneyder M <sneyder.martinez@wiga.io>
2023-02-25 22:51:07 -0500

    Scroll

--- js/main.js
+++ js/main.js
@@ -114,3 +114,44 @@ let swiper = new Swiper('.portfolio__container', {
       clickable: true,
     },
 });
+
+//===============================================
+const sections = document.querySelectorAll('section[id]');
+
+function scrollActive() {
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current => {
+        const [sectionHeight, sectionTop] = [current.offsetHeight, current.offsetTop - 50];
+        sectionId = current.getAttribute('id');
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link');
+        } else {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link');
+        }
+    });
+}
+window.addEventListener('scroll', scrollActive);
+
+//===============================================
+function scrollHeader() { 
+    const nav = document.getElementById('header');
+
+    if(this.scrollY >= 80) 
+        nav.classList.add('scroll-header'); 
+    else 
+        nav.classList.remove('scroll-header');
+}
+window.addEventListener('scroll', scrollHeader);
+
+//===============================================
+function scrollUp() {
+    const scrollUp = document.getElementById('scroll-up');
+
+    if(this.scrollY >= 560) 
+        scrollUp.classList.add('show-scroll'); 
+    else 
+        scrollUp.classList.remove('show-scroll');
+}
+window.addEventListener('scroll', scrollUp);
